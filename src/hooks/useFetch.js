d4bf11748c3534a14b7050262c45851d3cf6e58a
zaary <mohamedzaary@gmail.com>
2022-09-11 16:46:45 +0100

    fix

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,7 @@
 import  {useEffect , useState} from 'react';
 import axios from 'axios';
 
-export const API_KEY = ''
+export const API_KEY = '052f4caba8cd0b00eb10123b9d776298'
 
 function useFetch(url , method  , urlend=null ,  changing=null) {
     const [data, setData] = useState({data : '' , state : false});
