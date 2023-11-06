0d2f90a073c50beff20845074d878d66b25f3dac
Mohammed <82719618+Zaariy@users.noreply.github.com>
2023-11-06 09:18:10 +0000

    fix localstorage problem

--- src/utils/localstorage.js
+++ src/utils/localstorage.js
@@ -1,41 +1,35 @@
 function setToLocalStorage(name, value) {
-    // if localStorage is Empty
-    if (localStorage.length === 0) {
-        localStorage.setItem(name , [value] )
-        return
-    }
-    // check if item in array 
-    if (localStorage.getItem(name).includes(value) ) {
-        return
-    } else {
+  // if localStorage is Empty
+  if (localStorage.length === 0) {
+    localStorage.setItem(name, [value]);
+    return;
+  }
+  // check if item in array
 
-        const prv = localStorage.getItem(name);
-        localStorage.setItem(name , [...[prv] , value] )
-    }
+  if (!localStorage.getItem(name).includes(value)) {
+    const prv = localStorage.getItem(name);
+    localStorage.setItem(name, [...[prv], value]);
+  }
 }
 
 function getToLocalStorage(name) {
-    // return data as array
-    // we converted data from String to Array 
-    // using split() method
-    if (!localStorage.getItem('list_movies')) {
-        return false 
-    }
-    return localStorage.getItem(name).split(',');
+  // return data as array
+  // we converted data from String to Array
+  // using split() method
+  if (!localStorage.getItem("list_movies")) {
+    return false;
+  }
+  return localStorage.getItem(name).split(",");
 }
 function checkMovieOnList(id) {
-    // return True of False
-    if (!localStorage.getItem('list_movies')) {
-        return 
-    }
-    if (localStorage.getItem('list_movies').includes(id)) {
-        return true;
-    } else {
-        return false;
-    }
+  // return True of False
+  if (!localStorage.getItem("list_movies")) {
+    return;
+  }
+  if (localStorage.getItem("list_movies").includes(id)) {
+    return true;
+  } else {
+    return false;
+  }
 }
-export {
-    setToLocalStorage,
-    getToLocalStorage,
-    checkMovieOnList
-} 
+export { setToLocalStorage, getToLocalStorage, checkMovieOnList };
